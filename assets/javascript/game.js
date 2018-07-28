d2c7e6558db6e40c264036da51965ce365d7204f
Ben Pisaris <bpisaris@gmail.com>
2018-07-28 18:16:25 -0400

    fixed the clicks, now I gotta make the winHandler work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,77 +17,7 @@ var losses = 0;
 //set the game to not run
 var isGameRunning = false;
 
-//INITIALIZE
-function init() {
-    isGameRunning = true;
-    //Define a variable for your target score
-    var RandomTarget = Math.floor(Math.random * 500);
-    //generate values for the crystals
-    var crystal1 = Math.floor(Math.random() * 500);
-    var crystal2 = Math.floor(Math.random() * 300);
-    var crystal3 = Math.floor(Math.random() * 300);
-    var crystal4 = Math.floor(Math.random() * 300);
-    //define a variable for your score
-    var score = 0;
 
-}
-
-//Listening
-//had to change it to double click to init
-$(document).click(function() {
-    if (isGameRunning === false) {
-        init();
-    }
-});
-
-
-
-
-//these dont work on their own
-$("#red").click(function() {
-    console.log(crystal1);
-    var newScore = crystal1 + score;
-    score = newScore;
-    console.log(score);
-    winHandler();
-    newScore = 0;
-
-});
-$("#green").click(function() {
-    if (isGameRunning == true) {
-        console.log(crystal2);
-        var newScore = crystal2 + score;
-        score = newScore;
-        console.log(score);
-        winHandler();
-        newScore = 0;
-    }
-});
-$("#blue").click(function() {
-    console.log(crystal3);
-    var newScore = crystal3 + score;
-    score = newScore;
-    console.log(score);
-    winHandler();
-    newScore = 0;
-});
-$("#yellow").on("click", function() {
-    console.log(crystal4);
-    var newScore = crystal4 + score;
-    score = newScore;
-    console.log(score);
-    winHandler();
-    newScore = 0;
-});
-
-//have a function to add the crystal value to the score
-//inside the function, call the winHandler
-
-
-
-
-
-//end the game if the score hits the randomtarget or passes it
 function winHandler() {
     if (score === RandomTarget) {
         wins++;
@@ -100,3 +30,64 @@ function winHandler() {
         isGameRunning = false;
     }
 };
+
+//INITIALIZE
+function init() {
+    isGameRunning = true;
+    //Define a variable for your target score
+    var RandomTarget = Math.floor(Math.random * 500);
+    //generate values for the crystals
+    var crystal1 = Math.floor(Math.random() * 500);
+    var crystal2 = Math.floor(Math.random() * 300);
+    var crystal3 = Math.floor(Math.random() * 300);
+    var crystal4 = Math.floor(Math.random() * 300);
+    //define a variable for your score
+    var score = 0;
+    $("#red").click(function () {
+        //log the value of the crystal
+        console.log(crystal1);
+        //define a variable that adds the crystal to the score
+        var newScore = crystal1 + score;
+        score = newScore;
+        console.log(score);
+        winHandler();
+        newScore = 0;
+    
+    });
+    $("#green").click(function () {
+        if (isGameRunning == true) {
+            console.log(crystal2);
+            var newScore = crystal2 + score;
+            score = newScore;
+            console.log(score);
+            winHandler();
+            newScore = 0;
+        }
+    });
+    $("#blue").click(function () {
+        console.log(crystal3);
+        var newScore = crystal3 + score;
+        score = newScore;
+        console.log(score);
+        winHandler();
+        newScore = 0;
+    });
+    $("#yellow").on("click", function () {
+        console.log(crystal4);
+        var newScore = crystal4 + score;
+        score = newScore;
+        console.log(score);
+        winHandler();
+        newScore = 0;
+    });
+
+}
+
+//Listening
+$(document).click(function () {
+    if (isGameRunning === false) {
+        init();
+    }
+});
+//end the game if the score hits the randomtarget or passes it
+
